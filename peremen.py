5fa9496d9aab5ae4e3b971d8916cdfab79323aab
Artur <arturmeiers@gmail.com>
2022-11-05 20:13:24 +0200

    proverka

---
+++ peremen.py
@@ -0,0 +1,19 @@
+number1 = 1
+number2 = 3
+number3 = 3
+result = number1
+print(result)
+
+num1 = num2 = 5
+print(num1, num2)
+
+num_1, num_2 = 5,7
+print(num_1, num_2)
+
+swap1 = 8
+swap2 = 9
+swap1, swap2 = swap2, swap1 + swap2
+print(swap1, swap2)
+
+swap2 -= number1
+print(swap2)
---
+++ spisok peremen.py
@@ -0,0 +1,4 @@
+z, *x, c = [1, 1, 2, 3, 4, 5, 1, 8]
+print(z)
+print(x)
+print(c)
